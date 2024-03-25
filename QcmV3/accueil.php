8cb54c3d551c409019e1abc00b920444eda464d7
eSuardOdin <esuardodin@gmail.com>
2024-03-25 20:43:11 +0100

    Tableau de bord élève géré à la connexion

--- QcmV3/accueil.php
+++ QcmV3/accueil.php
@@ -3,14 +3,14 @@ session_start();
 include_once("navbar.php");
 if(!isset($_SESSION['page'])/* && $_SESSION['role'] == "Enseignant"*/)
 {
-    if($_SESSION['role'] == "Elève")
-    {
-        $_SESSION['page'] = "élève/tableau_bord.php";
-    }
-    else
-    {
+    // if($_SESSION['role'] == "Elève")
+    // {
+    //     $_SESSION['page'] = "élève/tableau_bord.php";
+    // }
+    // else
+    // {
         echo "<h1>Bienvenue sur l'application QCM</h1>";
-    }
+    // }
 }
 else
 {
